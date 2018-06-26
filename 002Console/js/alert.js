880cd02cd76f5b89d0d71be345be26d967d03995
Administrator <123456>
2018-06-26 11:25:37 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,17 @@
+function taolu() {
+    alert("你猜一会会发生什么");
+    alert("再等等");
+    alert("也许有奇迹");
+}
+
+alert("你怎么打开这个网页的");
+
+taolu();
+alert("你相信有奇迹吗");
+alert("让我问你一个问题");
+taolu();
+
+alert("果然没有奇迹");
+alert("我们就这样放弃吗");
+taolu();
+alert("这是最后一句了");
